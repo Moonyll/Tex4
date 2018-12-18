8fc8327588def351eed2d17617a57abb994c5497
Moonyll <s.pouwel@laposte.net>
2018-12-18 09:04:37 +0100

    Tex4

---
+++ Tex4'.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<string> shopping = new List<string>();
+        shopping.Add("Carotte");
+        shopping.Add("Oignons");
+        shopping.Add("Pomme de terre");
+        shopping.Add("Tomate");
+        for (int i = 0; i < shopping.Count; i++)
+        Console.WriteLine(shopping[i]);
+        }
+    }
+}
---
+++ Tex4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string[] shopping = new string[] { "Carotte", "Oignons", "Pommes de terre", "Salade", "Tomate" };
+         for (int i = 0; i < shopping.Length; i++)
+         Console.WriteLine(shopping[i]);
+
+        }
+    }
+}
